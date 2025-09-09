5994fed1ebd806a579043e631411e285da162fc5
BezotechestvoVM <bezotechestvovm@mer.ci.nsu.ru>
2025-09-09 08:58:13 +0700

    upd hme page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     return (
         <div className="h-full flex flex-col items-center justify-center gap-2">
             <div className="flex flex-col-reverse md:flex-row items-center gap-2">
-                <div className="flex flex-col max-w-[520px]">
+                <div className="flex flex-col max-w-[520px] gap-2">
                     <p className="">Web-Разработчик</p>
                     <p className="md:text-6xl text-5xl font-bold">Dexoron</p>
                     <p className="">
@@ -25,18 +25,18 @@ export default function Home() {
                         обеспечение и системные утилиты для Linux. Использую
                         операционную систему ArchLinux.
                     </p>
+                    <div className="flex flex-row">
+                        <a
+                            className="bg-white/10 hover:bg-white/20 dark:bg-black/10 dark:hover:bg-black/20 px-4 py-2 rounded-[8px] text-xl font-bold"
+                            href="https://github.com/dexoron"
+                        >
+                            GitHub
+                        </a>
+                    </div>
                 </div>
                 <div className="max-w-[320px] max-h-[320px]">
                     <img src={Logo} alt="" />
                 </div>
-            </div>
-            <div className="flex flex-row gap-2">
-                <a
-                    className="bg-white/10 hover:bg-white/20 dark:bg-black/10 dark:hover:bg-black/20 px-8 py-4 rounded-[8px] text-xl font-bold"
-                    href="https://github.com/dexoron"
-                >
-                    GitHub
-                </a>
             </div>
         </div>
     );
